0c15ee02c89a9a2a1a69ec149fd0c897f7749506
Azael <azaelmorales@hotmail.com>
2018-11-17 21:46:50 -0600

    list view

---
+++ app/src/main/java/mx/com/azaelmorales/yurtaapp/DetalleEmpleadoActivity.java
@@ -0,0 +1,25 @@
+package mx.com.azaelmorales.yurtaapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DetalleEmpleadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_empleado);
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar_panel_empleados);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("Empleados");
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),EmpleadosViewFragment.class));
+            }
+        });
+    }
+}
--- app/src/main/java/mx/com/azaelmorales/yurtaapp/EmpleadosViewFragment.java
+++ app/src/main/java/mx/com/azaelmorales/yurtaapp/EmpleadosViewFragment.java
@@ -1,12 +1,14 @@
 package mx.com.azaelmorales.yurtaapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -30,7 +32,7 @@ import java.util.ArrayList;
 public class EmpleadosViewFragment extends Fragment    {
 
     private ListView listView;
-    //private String [][] datos ;
+    private String [][] datos ;
     public EmpleadosViewFragment() {
 
     }
@@ -42,6 +44,13 @@ public class EmpleadosViewFragment extends Fragment    {
         View view = inflater.inflate(R.layout.fragment_empleados_view, container, false);
         listView = (ListView)view.findViewById(R.id.listViewEmpleados);
         cargarDatos();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent =new Intent(view.getContext(),DetalleEmpleadoActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
 
     }
@@ -88,7 +97,7 @@ public class EmpleadosViewFragment extends Fragment    {
         ArrayList <String> lista = new ArrayList<>();
         int longitud = jsonArray.length();
         int n = (longitud)/8;
-        String [][] datos = new String[n][3];
+        datos = new String[n][3];
             int j =0;
             for (int i=0; i<longitud; i+=8) {
                 datos[j][0] = jsonArray.getString(i+5);
